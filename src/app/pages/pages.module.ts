ca372756b65c0e217083e93f391626b3bb304f78
jamesgiraldo <jamesgiiraldo@gmail.com>
2021-01-16 00:56:11 -0500

    Fin seción 4 - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router' //esta es lo mismo para utilizar los modulos de rutas
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+// import { AppRoutingModule } from '../app-routing.module'; //esta es lo mismo para utilizar los modulos de rutas declaradas
+
+
+// Compnentes
+import { PagesComponent } from './pages/pages.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    // AppRoutingModule
+    RouterModule
+  ]
+})
+export class PagesModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Componentes
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
