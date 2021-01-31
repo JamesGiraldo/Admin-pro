0e07a5656ca219390b47a343f7103b34d8d64646
jamesgiraldo <jamesgiiraldo@gmail.com>
2021-01-30 22:29:29 -0500

    Fin seccion 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public titulo: string = "";
+  public tituloSubs$: Subscription;
+
+  constructor( private router: Router, private route: ActivatedRoute ) { 
+    this.tituloSubs$ = this.getArgumentosRuta()
+                          .subscribe( ({ titulo }) =>{
+                            this.titulo = titulo;   
+                            document.title = `AdminPro - ${ titulo }`;      
+                        });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+  
+  getArgumentosRuta(){
+    return this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map( (event: ActivationEnd) => event.snapshot.data ),
+      );    
+  }
 
   ngOnInit(): void {
   }
