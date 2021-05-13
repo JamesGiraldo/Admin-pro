0aadc1bd9156d50c1962e2b0aff6365cf2c79f86
James Giraldo <jamesgiiraldo@gmail.com>
2021-05-12 21:11:24 -0500

    Fin sesión 16

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -18,61 +18,61 @@ export class PerfilComponent implements OnInit {
   public perfilForm: FormGroup;
   public usuario: Usuario;
   public imagenSubir: File;
-  public imgTemp: any  = null;
-  
-  constructor( private fb: FormBuilder, private usuarioService: UsuarioService, private fileUploadService: FileUploadService) {  
+  public imgTemp: any = null;
+
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService, private fileUploadService: FileUploadService) {
     this.usuario = usuarioService.usuario;
   }
-  
+
   ngOnInit(): void {
-    this.perfilForm  = this.fb.group({ 
-      nombre: [ this.usuario.nombre  , Validators.required],
-      email: [ this.usuario.email , [ Validators.required, Validators.email ] ],
+    this.perfilForm = this.fb.group({
+      nombre: [this.usuario.nombre, Validators.required],
+      email: [this.usuario.email, [Validators.required, Validators.email]],
     });
   }
 
   actualiarPerfil() {
-    console.log( this.perfilForm.value );
-    this.usuarioService.actualizarPerfil( this.perfilForm.value )
-                      .subscribe( () => {
-                        const { nombre, email } = this.perfilForm.value;
-                        this.usuario.nombre = nombre;
-                        this.usuario.email = email;
-                        /** Si sucede un ok renderizo la respuesta con el sweetalert2 */
-                        Swal.fire('Guardado', `Cambios fueron guardados...`, 'success')
-                      }, (err) => {
-                         /** Si sucede un error renderizo la respuesta con el sweetalert2 */
-                         Swal.fire('Oops...', `${ err.error.msg }`, 'error')
-                      });
-  }
-  
-  cambiarImagen( file: File ): any {
-          
-      this.imagenSubir = file;
-      
-      /** si la imagen no existe, no hacer nada XD */
-      if (!file) {
-        return this.imgTemp = null;
-      }
-  
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-  
-      reader.onloadend = () => {
-        this.imgTemp = reader.result;
-      }
+    console.log(this.perfilForm.value);
+    this.usuarioService.actualizarPerfil(this.perfilForm.value)
+      .subscribe(() => {
+        const { nombre, email } = this.perfilForm.value;
+        this.usuario.nombre = nombre;
+        this.usuario.email = email;
+        /** Si sucede un ok renderizo la respuesta con el sweetalert2 */
+        Swal.fire('Guardado', `Cambios fueron guardados...`, 'success')
+      }, (err) => {
+        /** Si sucede un error renderizo la respuesta con el sweetalert2 */
+        Swal.fire('Oops...', `${err.error.msg}`, 'error')
+      });
   }
 
-  subirImagen(){
-    this.fileUploadService.actualizarFoto( this.imagenSubir, 'usuarios', this.usuario.uid )
-                          .then( img => {
-                            this.usuario.img = img;
-                            /** Si sucede un ok renderizo la respuesta con el sweetalert2 */
-                            Swal.fire('¡Muy bien!', `Cambios de avatar exitosamente...`, 'success')
-                          }).catch ( err => {
-                            /** Si sucede un error renderizo la respuesta con el sweetalert2 */
-                            Swal.fire('Oops...', `${ err.error.msg }`, 'error')
-                          });
+  cambiarImagen(file: File) {
+
+    this.imagenSubir = file;
+
+    /** si la imagen no existe, no hacer nada XD */
+    if (!file) {
+      return this.imgTemp = null;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      this.imgTemp = reader.result;
+    }
+  }
+
+  subirImagen() {
+    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
+      .then(img => {
+        this.usuario.img = img;
+        /** Si sucede un ok renderizo la respuesta con el sweetalert2 */
+        Swal.fire('¡Muy bien!', `Cambios de avatar exitosamente...`, 'success')
+      }).catch(err => {
+        /** Si sucede un error renderizo la respuesta con el sweetalert2 */
+        Swal.fire('Oops...', `${err.error.msg}`, 'error')
+      });
   }
 
 }
